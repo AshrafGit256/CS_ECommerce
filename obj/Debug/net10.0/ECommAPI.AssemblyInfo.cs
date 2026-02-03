4d405617ff928332425fa269e447c3823fe0f199
AshrafGit256 <ashrafwalusimbi27@gmail.com>
2026-02-03 15:25:34 +0300

    Removed the faulty migrations

--- obj/Debug/net10.0/ECommAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/ECommAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02795e7d04c604ce1d5355a845db57f81df799b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2ba98f57adda5e8b2145925339c15c4616eb62f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
